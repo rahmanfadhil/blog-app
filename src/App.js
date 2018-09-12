285b9cf5953f7fa0a3165d69a50f487214dd04e4
Rahman Fadhil <rhmnfadhil@gmail.com>
2018-09-12 11:33:12 +0700

    create ui design

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
 import React from "react";
+import {
+  Header,
+  Container,
+  Left,
+  Button,
+  Icon,
+  Body,
+  Title,
+  Right,
+  Fab,
+  Card,
+  CardItem,
+  H4
+} from "native-base";
 import { View, Text } from "react-native";
 
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name="menu" />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Blog App</Title>
+          </Body>
+          <Right>
+            <Button transparent>
+              <Icon name="refresh" />
+            </Button>
+          </Right>
+        </Header>
+        <View style={{ flex: 1, padding: 10 }}>
+          <Card>
+            <CardItem>
+              <Text style={{ fontSize: 19, fontWeight: "500" }}>
+                Learn React Native
+              </Text>
+            </CardItem>
+          </Card>
+          <Fab
+            position="bottomRight"
+            style={{ backgroundColor: "#5067FF" }}
+            onPress={() => console.log("HELLO")}
+          >
+            <Icon name="add" />
+          </Fab>
+        </View>
+      </Container>
     );
   }
 }
